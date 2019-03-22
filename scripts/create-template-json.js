f1209e891ced2742caecfbf2153a1c51b4c15581
Patrick Barnes <patrick@devinsites.com>
2019-03-22 14:21:18 -0700

    fix: set RoleArn via env var

--- scripts/create-template-json.js
+++ scripts/create-template-json.js
@@ -8,5 +8,7 @@ template.templateBody.Resources.policy.Properties.PolicyDocument = JSON.stringif
   template.templateBody.Resources.policy.Properties.PolicyDocument
 );
 template.templateBody.Resources.thing.Properties.AttributePayload = { stage };
+console.log(template.roleArn)
+template.roleArn = process.env.JITP_ROLE_ARN;
 template.templateBody = JSON.stringify(template.templateBody);
 fs.writeFileSync("provisioning-template.json", JSON.stringify(template));
