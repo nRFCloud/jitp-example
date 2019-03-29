593b4d0437fbaf17ad0e7c1bdd7cf30bf7451b86
Patrick Barnes <patrick@devinsites.com>
2019-03-29 14:49:32 -0700

    style: prettify

--- scripts/create-template-json.js
+++ scripts/create-template-json.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const rawdata = fs.readFileSync("provisioning-template.js");
+const rawdata = fs.readFileSync('provisioning-template.js');
 const template = JSON.parse(rawdata);
 const stage = process.env.STAGE || 'dev';
 
 template.templateBody.Resources.policy.Properties.PolicyDocument = JSON.stringify(
-  template.templateBody.Resources.policy.Properties.PolicyDocument
+  template.templateBody.Resources.policy.Properties.PolicyDocument,
 );
 template.templateBody.Resources.thing.Properties.AttributePayload = { stage };
 template.roleArn = process.env.JITP_ROLE_ARN;
 template.templateBody = JSON.stringify(template.templateBody);
-fs.writeFileSync("provisioning-template.json", JSON.stringify(template));
+fs.writeFileSync('provisioning-template.json', JSON.stringify(template));
